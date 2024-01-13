b6308324f685c5c50ca7b0bbb5626a84740cc20e
Zayaan Khan <87833336+zkhan122@users.noreply.github.com>
2024-01-13 14:08:25 +0000

    Create Interval.java

---
+++ src/greedy/WeightedIntervalScheduling/Interval.java
@@ -0,0 +1,37 @@
+public class Interval {
+
+
+    private int start;
+    private int finish;
+    private int weight;
+
+    public Interval(int start, int finish, int weight) {
+        this.start = start;
+        this.finish = finish;
+        this.weight = weight;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getFinish() {
+        return finish;
+    }
+
+    public void setFinish(int finish) {
+        this.finish = finish;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
